3d3fe1893e1b74b9633bea2f9174fc4ce4943277
madongchan <mdc0078@gmail.com>
2021-07-23 21:18:44 +0900

    Whac-A-Mole
    a

---
+++ DuDaGi_VR/Assets/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum GameState
+{
+    Ready,
+    Play,
+    End
+}
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameState Gs;
+    public AudioClip readySound;
+    public AudioClip goSound;
+    public int score;
+    public float limitTime;
+    public TextMeshPro timeText;
+    public TextMeshPro scoreText;
+    public GameObject black;
+    public TextMeshPro endScore;
+    public TextMeshPro highScore;
+    public GameObject finalWindow;
+
+
+    AudioSource audioSouce;
+    
+    void Start()
+    {
+        audioSouce = GetComponent<AudioSource>();
+        audioSouce.clip = readySound;
+        audioSouce.Play();
+    }
+    public void Go()
+    {
+        Gs = GameState.Play;
+        audioSouce.clip = goSound;
+        audioSouce.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Gs == GameState.Play)
+        {
+            limitTime -= Time.deltaTime;
+            if(limitTime <= 0)
+            {
+                limitTime = 0;
+                GameOver();
+            }
+            timeText.text = limitTime.ToString("N2");
+            scoreText.text = score.ToString();
+        }
+    }
+
+    void GameOver()
+    {
+        iTween.FadeTo(black, iTween.Hash("alpha", 180, "delay", 0.1f, "time", 0.5f));
+        Gs = GameState.End;
+
+        iTween.MoveTo(finalWindow, iTween.Hash("y", 0, "delay", 0.5f, "time", 0.5f));
+
+        if (score > PlayerPrefs.GetInt("BS"))
+        {
+            PlayerPrefs.SetInt("BS", score);
+        }
+
+        endScore.text = score.ToString();
+        highScore.text = PlayerPrefs.GetInt("BS").ToString();
+    }
+}
---
+++ DuDaGi_VR/Assets/Hole.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    Animator ani;
+    bool touchPossible = false;
+    AudioSource audioSource;
+    public AudioClip openSound;
+    public AudioClip catchSound;
+    public GameManager GM;
+    bool bomb = false;
+    
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Open()
+    {
+        touchPossible = true;
+        audioSource.clip = openSound;
+        audioSource.Play();
+
+        if(GM.Gs == GameState.Ready)
+        {
+            GM.Go();
+            
+        }
+    }
+    public void Close()
+    {
+        touchPossible = false;
+    }
+    private void OnMouseDown()
+    {
+        if(touchPossible)
+        {
+            touchPossible = false;
+            ani.SetTrigger("isTouch");
+            audioSource.clip = catchSound;
+            audioSource.Play();
+
+            if (!bomb)
+            {
+                GM.score += 100;
+            }
+            else
+            {
+                GM.score -= 500;
+            }
+        }
+    }
+    public IEnumerator End()
+    {
+        float randomTime = Random.Range(1.0f, 3.0f);
+        float randomD = Random.Range(1.0f, 10.0f);
+        yield return new WaitForSeconds(randomTime);
+
+        if(GM.Gs != GameState.End)
+        {
+            if (randomD >= 5.0f)//20%
+            {
+                bomb = true;
+                ani.SetTrigger("Bomb");
+            }
+            else
+            {
+                bomb = false;
+                ani.SetTrigger("Open");
+            }
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
